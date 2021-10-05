41abc7bd9251c42eee583be600ceb73888b26cbc
Heallii <90777525+Heallii@users.noreply.github.com>
2021-10-05 23:53:23 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Задание_на_for
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.posl = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите последовательность\r\n         натуральных чисел: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(309, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // posl
+            // 
+            this.posl.Location = new System.Drawing.Point(229, 161);
+            this.posl.Name = "posl";
+            this.posl.Size = new System.Drawing.Size(101, 23);
+            this.posl.TabIndex = 2;
+            this.posl.Text = "Определить";
+            this.posl.UseVisualStyleBackColor = true;
+            this.posl.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(486, 45);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Задание 2:\r\nДана последовательность натуральных чисел, в котором все цифры различ" +
+    "ны. \r\nОпределить, какая цифра расположена в ней левее: максимальная или минималь" +
+    "ная.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(229, 190);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(101, 23);
+            this.clear.TabIndex = 4;
+            this.clear.Text = "Очистить";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 278);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.posl);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button posl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_на_for
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Задача на for";  // заголовок формы
+            textBox1.Text = Properties.Settings.Default.chisla.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // кнопка "определить"
+        {
+
+            string chisla = Convert.ToString(this.textBox1.Text);
+            MessageBox.Show(Logic.MainLogic(chisla));
+            Properties.Settings.Default.chisla = chisla;
+            Properties.Settings.Default.Save();  // автосохранение
+
+
+
+        }
+
+        private void clear_Click(object sender, EventArgs e) // кнпока "очистить"
+        {
+            textBox1.Text = "";
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) // защита данных
+        {
+
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number))
+            {
+                e.Handled = true;
+            }
+  
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Logic
+    {
+        public static string MainLogic(string chisla)
+
+        {
+            string OutMessage;         // переменная для сообщения
+            int min = Convert.ToInt32(chisla[1]); // преобразование в битовое экв. число
+            int max = 0;             // вводим числа и позиции
+            int min_pos = 0;
+            int max_pos = 0;
+
+            for (int i = 1; i < chisla.Length; i++)
+            {
+                if (Convert.ToInt32(chisla[i]) < min)
+                {
+                    min = Convert.ToInt32(chisla[i]);
+                    min_pos = i;
+                }
+                if (Convert.ToInt32(chisla[i]) > max)
+                {
+                    max = Convert.ToInt32(chisla[i]);
+                    max_pos = i;
+                }
+            }
+            if (max_pos < min_pos)
+                return OutMessage = "Максимальное число "+ chisla[0] + " левее!"; // собственно тут была ошибка с выводом, которую вы записали в блокнот, я её починил
+            else
+                return OutMessage = "Минимальное число " + chisla[0] + " левее!"; // ну и тут :)
+        }
+    }
+
+}
